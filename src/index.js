82d5893ef7c8224beabe4abc97e815157ab57728
Ivan Gagarinov <dzencot@gmail.com>
2021-09-23 21:52:51 +0500

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 const getSquare = (number) => number ** 2;
 
+const squareOfSum = (number) => {
+  const results = [];
+  for (let i = 0; i <= 10; i++) {
+    results.push(getSquare(number * 2) + 1);
+  }
+
+  return results;
+};
+
+export { squareOfSum };
 export default getSquare;
